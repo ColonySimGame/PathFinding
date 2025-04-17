53d82809a1350c48b571dc320b4838ce50aa0c9d
nemuruyama <nemuruyama@gmail.com>
2025-04-17 23:58:25 +0200

    Benchmark

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿using PathFindingGrid.Grid;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using PathFindingGrid.Grid;
 using PathFindingGrid.PathFinding;
 
 namespace PathFindingGrid;
 
 public abstract class Program
 {
-    public static void Main(string[] args)
+    public AStarSearch? AStar;
+    
+    [GlobalSetup]
+    public void Setup()
     {
         const int width = 250;
         const int height = 250;
@@ -15,9 +20,9 @@ public abstract class Program
         
         // Initialize map (example: all walkable)
         for (var z = 0; z < depth; z++)
-            for (var y = 0; y < height; y++)
-                for (var x = 0; x < width; x++)
-                    map.Tiles[x, y, z] = new Tile(new Point3D(x, y, z));
+        for (var y = 0; y < height; y++)
+        for (var x = 0; x < width; x++)
+            map.Tiles[x, y, z] = new Tile(new Point3D(x, y, z));
 
         // Add some obstacles
         map.Tiles[5, 5, 0].Walkable = false;
@@ -50,45 +55,25 @@ public abstract class Program
         {
             map.AddTeleporter(rawTeleporter);
         }
-
-        // --- Initialize A* ---
-        var astar = new AStarSearch(width, height, depth, map, allowDiagonal: true);
-
+        
+        AStar = new AStarSearch(width, height, depth, map, allowDiagonal: true);
+    }
+    
+    [Benchmark]
+    public void AStarBenchmark()
+    {
         // --- Define Start and Goal ---
         var start = new Point3D(0, 0, 0);
         var goal = new Point3D(9, 9, 4);
 
-        Console.WriteLine($"Finding path from {start} to {goal}");
-
         // --- Find Path ---
-        var startTime = DateTime.Now.Ticks;
-        List<Point3D>? path = astar.FindPath(start, goal);
-        var endTime = DateTime.Now.Ticks;
-        Console.WriteLine($"Time taken: {(endTime - startTime) / 10000} ms");
-
-        // --- Output Path ---
-        if (path is not null)
-        {
-            Console.WriteLine("Path found:");
-            Console.WriteLine(string.Join(" -> ", path));
-            // Calculate path cost (optional)
-            double totalCost = 0;
-            startTime = DateTime.Now.Ticks;
-            for(var i = 0; i < path.Count - 1; i++)
-            {
-                var origin = path[i];
-                var target = path[i+1];
-                var tile = map.GetTile(target);
-                totalCost += AStarSearch.CalculateMoveCost(origin, target, map.TeleportersByPoint, tile);
-            }
-            endTime = DateTime.Now.Ticks;
-            Console.WriteLine($"Time taken: {(endTime - startTime) / 10000} ms");
-            Console.WriteLine($"Approximate Path Cost: {totalCost}"); // Note: This simple calc might slightly differ from A*'s internal G-score due to heuristic influence/tie-breaking.
-        }
-        else
-        {
-            Console.WriteLine("Path could not be found.");
-        }
+        AStar?.FindPath(start, goal);
+    }
+    
+    public static void Main(string[] args)
+    {
+        // Start benchmarking
+        BenchmarkRunner.Run<Program>();
     }
     
 }
